81d7fee0e331c276340844038031978758e71526
Yao Shuai <ys3721@hotmail.com>
2021-03-16 11:41:23 +0800

    [XBtc]Chinese liquor stock that drew comparisons to bitcoin is clinging to 2020 gains

--- src/main/java/cn/alip8/algorithm/recursion/AllStringSubsequences.java
+++ src/main/java/cn/alip8/algorithm/recursion/AllStringSubsequences.java
@@ -1,8 +1,10 @@
 package cn.alip8.algorithm.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author: Yao Shuai
@@ -10,26 +12,48 @@ import java.util.List;
  */
 public class AllStringSubsequences {
 
+    public static List<String> subsRepeat(String s) {
+        char[] str = s.toCharArray();
+        String path = "";
+        ArrayList<String> list = new ArrayList<>();
+        process(str, 0, list, path);
+        return list;
+    }
+
+    public static void process(char[] str, int index, List<String> list, String path) {
+        if (index == str.length) {
+            list.add(path);
+            return;
+        }
+        process(str, index + 1, list, path);
+        process(str, index + 1, list, path + String.valueOf(str[index]));
+    }
+
     public static List<String> subsNoRepeat(String s) {
         char[] str = s.toCharArray();
         String path = "";
-        HashSet<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         process2(str, 0, set, path);
-        List<String> ans = new ArrayList<>();
-        for (String cur : set) {
-            ans.add(cur);
-        }
-        return ans;
+        return new ArrayList<String>(Arrays.asList(set.toArray(new String[0])));
     }
 
-    public static void process2(char[] str, int index, HashSet<String> set, String path) {
+
+    public static void process2(char[] str, int index, Set<String> set, String path) {
         if (index == str.length) {
             set.add(path);
             return;
         }
-        String no = path;
-        process2(str, index + 1, set, no);
-        String yes = path + String.valueOf(str[index]);
-        process2(str, index + 1, set, yes);
+        process2(str, index + 1, set, path);
+        process2(str, index + 1, set, path + String.valueOf(str[index]));
     }
+
+    public static void main(String[] args) {
+        String test = "pcrdhwdxmqdznbenhwjsenjhvulyve";
+        List<String> ans1 = subsRepeat(test);
+        System.out.println(ans1.contains(""));
+        for (String str : ans1) {
+            System.out.println(str);
+        }
+    }
+
 }
