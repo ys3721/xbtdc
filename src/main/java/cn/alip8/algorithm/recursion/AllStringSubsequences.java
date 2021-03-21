469988341c54a2de05d8c2d32c646db45e6de6a3
Yao Shuai <ys3721@hotmail.com>
2021-03-21 23:05:50 +0800

    [XBtc]Chinese liquor stock that drew comparisons to bitcoin is clinging to 2020 gains

--- src/main/java/cn/alip8/algorithm/recursion/AllStringSubsequences.java
+++ src/main/java/cn/alip8/algorithm/recursion/AllStringSubsequences.java
@@ -47,12 +47,44 @@ public class AllStringSubsequences {
         process2(str, index + 1, set, path + String.valueOf(str[index]));
     }
 
+    public static ArrayList<String> permutation(String str) {
+        ArrayList<String> res = new ArrayList<>();
+        if (str == null || str.length() == 0) {
+            return res;
+        }
+        char[] chs = str.toCharArray();
+        process(chs, 0, res);
+        return res;
+    }
+
+    public static void process(char[] str, int i, ArrayList<String> ans) {
+        if (i == str.length) {
+            ans.add(String.valueOf(str));
+        }
+        for (int j = i; j < str.length; j++) {
+            swap(str, i, j);
+            process(str, i + 1, ans);
+            swap(str, i, j);
+        }
+    }
+
+    public static void swap(char[] chars, int i , int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+
     public static void main(String[] args) {
-        String test = "pcrdhwdxmqdznbenhwjsenjhvulyve";
+        String test = "pcrd";
         List<String> ans1 = subsRepeat(test);
         System.out.println(ans1.contains(""));
         for (String str : ans1) {
             System.out.println(str);
+        }
+        System.out.println("------------------");
+        List<String> ans2 = permutation(test);
+        for (String str2 : ans2) {
+            System.out.println(str2);
         }
     }
 
