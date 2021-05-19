aba835bd38b4a8f8d8e7258ca50540673eec136e
Yao Shuai <ys3721@hotmail.com>
2021-05-19 21:10:41 +0800

    欲思其利,必虑其害,欲思其成,必虑其败

---
+++ src/main/java/cn/alip8/io/webproxy/WebProxyInitializer.java
@@ -0,0 +1,27 @@
+package cn.alip8.io.webproxy;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author: Yao Shuai
+ * @date: 2021/5/18 14:42
+ */
+public class WebProxyInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final String remoteHost;
+    private final int remotePort;
+
+    // As we use inboundChannel.eventLoop() when building the Bootstrap this does not need to be volatile as
+    // the outboundChannel will use the same EventLoop (and therefore Thread) as the inboundChannel.
+    private Channel outboundChannel;
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast(new LineBasedFrameDecoder(Integer.MAX_VALUE), new LoggingHandler(LogLevel.INFO), new WebProxyFrontendHandler());
+    }
+}
--- src/main/java/cn/alip8/io/webproxy/WebProxyServer.java
+++ src/main/java/cn/alip8/io/webproxy/WebProxyServer.java
@@ -24,9 +24,9 @@ public class WebProxyServer {
             ServerBootstrap b = new ServerBootstrap();
             b.group(boss, worker)
                     .channel(NioServerSocketChannel.class)
-                    .handler(new WebProxyInitiailzer())
-                    .option(ChannelOption.SO_BACKLOG, 150);
-                    //.childHandler(ChannelOption.AUTO_READ, false);
+                    .childHandler(new WebProxyInitializer())
+                    .option(ChannelOption.SO_BACKLOG, 150)
+                    .childOption(ChannelOption.AUTO_READ, false);
             b.bind(localPort).sync().channel().closeFuture().sync();
         } finally {
             boss.shutdownGracefully();
